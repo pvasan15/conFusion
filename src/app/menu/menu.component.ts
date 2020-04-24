279b9cd22bbcca3e4722083ee3ae854c8bb242d6
Prabha Srinivasan <pvasan15@yahoo.com>
2020-04-24 16:03:49 -0400

    Animations part 2.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,11 +3,19 @@ import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Router } from '@angular/router';
 import { inject } from '@angular/core/testing';
+import { flyInOut, expand } from '../animations/app.animation';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(), expand()
+  ]
 })
 export class MenuComponent implements OnInit {
   dishes: Dish[];
